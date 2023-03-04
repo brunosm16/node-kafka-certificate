3aa8721f8645555fce388dacf136e77e0144b2b1
brunosm16 <brunoskr23@gmail.com>
2023-03-04 20:59:37 -0300

    feat: connect kafka

--- api/src/router/routes.ts
+++ api/src/router/routes.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
 import express from 'express';
+import { ServerError } from '@/errors/server-error';
 
 const routes = express.Router();
 
 routes.post('/certificate', async (req: Request, res: Response) => {
-  return res.json({ ok: 'Route Certificate is OK' });
+  try {
+    const { producer } = req;
+
+    await producer.connect();
+
+    return res.json({ ok: 'Route Certificate is OK' });
+  } catch (err) {
+    throw new ServerError(err);
+  }
 });
 
 export default routes;
--- api/src/utils/set-kafka-client.ts
+++ api/src/utils/set-kafka-client.ts
@@ -2,13 +2,9 @@ import { makeKafkaClient } from '@/factories/kafka-client';
 import { NextFunction, Request } from 'express';
 
 export default function (req: Request, next: NextFunction) {
-  const kafkaClient = makeKafkaClient('clientId', ['kafka:9092']);
+  const kafkaClient = makeKafkaClient('api', ['localhost:9092']);
 
-  const { body } = req;
-
-  if (body) {
-    req.body.producer = kafkaClient.producer();
-  }
+  req.producer = kafkaClient.producer();
 
   return next();
 }
